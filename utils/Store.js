8389f656f70d047ef9c6386fec3e12d7575be19c
Vai 1337 <115883642+Vai-1337@users.noreply.github.com>
2023-02-18 20:20:56 +0100

    done

--- utils/Store.js
+++ utils/Store.js
@@ -1,37 +1,41 @@
-import {  createContext, useReducer } from 'react';
+import { createContext, useReducer } from 'react';
+import Cookie from 'js-cookie';
 
 export const Store = createContext();
 
 const initialState = {
-    cart: { cartItems: [] },
+  cart: Cookie.get('cart') ? JSON.parse(Cookie.get('cart')) : { cartItems: [] },
 };
 
 function reducer(state, action) {
-
-    switch (action.type) {
-        case 'CART_ADD_ITEM': {
-            const newItem = action.payload;
-            const existItem = state.cart.cartItems.find(
-                (item) => item.slug === newItem.slug
-            );
-            const cartItems = existItem? state.cart.cartItems.map((item) => item.name === existItem.name ? newItem : item) 
-            : 
-            [...state.cart.cartItems, newItem];
-            return  {...state, cart: { ...state.cart, cartItems } };
-        }
-        case 'REMOVE_FROM_CART': {
-            const cartItems = state.cart.cartItems.filter(
-                (item) => item.slug !== action.payload.slug
-            );
-            return { ...state, cart: { ...state.cart, cartItems } };
-        }
-        default: 
-        return state;
-        }
+  switch (action.type) {
+    case 'CART_ADD_ITEM': {
+      const newItem = action.payload;
+      const existItem = state.cart.cartItems.find(
+        (item) => item.slug === newItem.slug
+      );
+      const cartItems = existItem
+        ? state.cart.cartItems.map((item) =>
+            item.name === existItem.name ? newItem : item
+          )
+        : [...state.cart.cartItems, newItem];
+      Cookie.set('cart', JSON.stringify({ ...state.cart, cartItems }));
+      return { ...state, cart: { ...state.cart, cartItems } };
     }
+    case 'REMOVE_FROM_CART': {
+      const cartItems = state.cart.cartItems.filter(
+        (item) => item.slug !== action.payload.slug
+      );
+      Cookie.set('cart', JSON.stringify({ ...state.cart, cartItems }));
+      return { ...state, cart: { ...state.cart, cartItems } };
+    }
+    default:
+      return state;
+  }
+}
 
-export function StoreProvider( {children }) {
-    const [state, dispatch] = useReducer(reducer, initialState);
-    const value = { state, dispatch };
-    return <Store.Provider value={value}>{children}</Store.Provider>;
+export function StoreProvider({ children }) {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const value = { state, dispatch };
+  return <Store.Provider value={value}>{children}</Store.Provider>;
 }
