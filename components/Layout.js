a25c4e79471c45e92a59199bb7462136a49bb2ca
Vai 1337 <115883642+Vai-1337@users.noreply.github.com>
2023-02-19 01:38:24 +0100

    login

--- components/Layout.js
+++ components/Layout.js
@@ -2,14 +2,19 @@ import { Store } from '../utils/Store';
 import Head from 'next/head';
 import Link from 'next/link';
 import React, { useContext, useEffect, useState } from 'react';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { useSession } from 'next-auth/react';
 
 const Layout = ({ title, children }) => {
+  const { status, data: session } = useSession();
+
   const { state, dispatch } = useContext(Store);
   const { cart } = state;
-  const [cartItemsCount, setCartItemsCount] = useState(0)
+  const [cartItemsCount, setCartItemsCount] = useState(0);
   useEffect(() => {
-    setCartItemsCount(cart.cartItems.reduce((a, c) => a + c.quantity, 0))
-  }, [cart.cartItems])
+    setCartItemsCount(cart.cartItems.reduce((a, c) => a + c.quantity, 0));
+  }, [cart.cartItems]);
 
   return (
     <>
@@ -18,6 +23,9 @@ const Layout = ({ title, children }) => {
         <meta name="description" content="Miner Shop" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
+      <ToastContainer position="bottom-center" limit={1} />
+
       <div className="flex min-h-screen flex-col justify-between">
         <header>
           <nav className="flex h-12 items-center px-4 justify-between shadow-md">
@@ -26,7 +34,8 @@ const Layout = ({ title, children }) => {
             </Link>
             <div className="flex">
               <Link href="/cart">
-                <p className="p-2">Panier
+                <p className="p-2">
+                  Panier
                   {cartItemsCount > 0 && (
                     <span className="ml-1 rounded-full bg-red-600 px-2 py-1 text-xs font-bold text-white">
                       {cartItemsCount}
@@ -34,9 +43,16 @@ const Layout = ({ title, children }) => {
                   )}
                 </p>
               </Link>
-              <Link href="/login">
-                <p className="p-2">Connexion</p>
-              </Link>
+
+              {status === 'loading' ? (
+                'loading'
+              ) : session?.user ? (
+                <p className="p-2">{session.user.name}</p>
+              ) : (
+                <Link href="/login">
+                  <p className="p-2">Connexion</p>
+                </Link>
+              )}
             </div>
           </nav>
         </header>
--- pages/login.js
+++ pages/login.js
@@ -1,69 +1,93 @@
 import Link from 'next/link';
-import React from 'react';
-import Layout from '../components/Layout';
+import React, { useEffect } from 'react';
+import { signIn, useSession } from 'next-auth/react';
 import { useForm } from 'react-hook-form';
+import Layout from '../components/Layout';
+import { getError } from '../utils/error';
+import { toast } from 'react-toastify';
+import { useRouter } from 'next/router';
 
-const LoginScreen = () => {
+export default function LoginScreen() {
+  const { data: session } = useSession();
+
+  const router = useRouter();
+  const { redirect } = router.query;
+
+  useEffect(() => {
+    if (session?.user) {
+      router.push(redirect || '/');
+    }
+  }, [router, session, redirect]);
+
   const {
     handleSubmit,
     register,
     formState: { errors },
   } = useForm();
-  const submitHandler = ({ email, password }) => {};
-
+  const submitHandler = async ({ email, password }) => {
+    try {
+      const result = await signIn('credentials', {
+        redirect: false,
+        email,
+        password,
+      });
+      if (result.error) {
+        toast.error(result.error);
+      }
+    } catch (err) {
+      toast.error(getError(err));
+    }
+  };
   return (
-    <Layout title="Se connecter">
+    <Layout title="Login">
       <form
         className="mx-auto max-w-screen-md"
         onSubmit={handleSubmit(submitHandler)}
       >
-        <h1 className="mb-4 text-xl">Se connecter</h1>
+        <h1 className="mb-4 text-xl">Login</h1>
         <div className="mb-4">
           <label htmlFor="email">Email</label>
           <input
             type="email"
             {...register('email', {
-              required: 'Entrer une adresse email',
+              required: 'Please enter email',
               pattern: {
-                value: /^[a-zA-z0-9_.+-]+@[a-zA-Z0-9-.]+$/i,
-                message: 'Adresse email non valide',
+                value: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$/i,
+                message: 'Please enter valid email',
               },
             })}
             className="w-full"
             id="email"
-            autofocus
+            autoFocus
           ></input>
           {errors.email && (
             <div className="text-red-500">{errors.email.message}</div>
           )}
         </div>
         <div className="mb-4">
-          <label htmlFor="password">Mot de passe</label>
+          <label htmlFor="password">Password</label>
           <input
             type="password"
             {...register('password', {
-                required: 'Entrer un mot de passe',
-                minLength: { value: 6, message: 'Le mot de passe doit contenir au moins 6 caractères' },
-                })}
+              required: 'Please enter password',
+              minLength: { value: 6, message: 'password is more than 5 chars' },
+            })}
             className="w-full"
             id="password"
-            autofocus
+            autoFocus
           ></input>
-            {errors.password && (
-            <div className="text-red-500">{errors.password.message}</div>
-            )}
+          {errors.password && (
+            <div className="text-red-500 ">{errors.password.message}</div>
+          )}
         </div>
-        <div className="mb-4">
-          <button className="primary-button">Se connecter</button>
+        <div className="mb-4 ">
+          <button className="primary-button">Login</button>
         </div>
-        <div className="mb-4">
-          {' '}
-          Vous n&apost;avez pas de compte ? &nbsp;
-          <Link href="/register">Créer un compte</Link>
+        <div className="mb-4 ">
+          Don&apos;t have an account? &nbsp;
+          <Link href={`/register?redirect=${redirect || '/'}`}>Register</Link>
         </div>
       </form>
     </Layout>
   );
-};
-
-export default LoginScreen;
+}
